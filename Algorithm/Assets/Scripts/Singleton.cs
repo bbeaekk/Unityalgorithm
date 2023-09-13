8b93e6efa35fe58f90f328e3d88e445671ce2c35
bbeaekk <snowoman404@gmail.com>
2023-09-13 15:56:06 +0900

    feat: "SGTransformExtention 구현"
    Transform 초기화 함수들과 Eular 축 회전 함수 제작

---
+++ Algorithm/Assets/Scripts/Singleton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+   public static Singleton Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public int playerScore = 0;
+    public void InscreaseScore(int amount)
+    {
+        playerScore += amount;
+    }
+
+}
